b91049187dea28d8d762eb05685846db50be5f4a
OksanaNova <o.nova0804@gmail.com>
2025-01-20 13:59:39 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Accordion } from './Accordion/Accordion';
+import './App.css'
+
+function App() {
+
+
+  return (
+    <div>
+      <Accordion />
+    </div>
+
+  )
+}
+
+export default App
