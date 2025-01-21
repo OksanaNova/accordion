070b893560c42a7f8e7f13df86ff468dfc5ea9cb
OksanaNova <o.nova0804@gmail.com>
2025-01-21 10:37:09 -0700

    tabs added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
+import { useState } from 'react';
 import { Accordion } from './Accordion/Accordion';
 import './App.css'
+import Tabs from './Tabs/Tabs';
+import TabInfoOne from './Tabs/TabInfoOne';
+import TabInfoTwo from './Tabs/TabInfoTwo';
+import TabInfoThree from './Tabs/TabInfoThree';
 
 function App() {
+
+  const [activeTab, setActiveTab] = useState(0);
 
 
   return (
     <div>
       <Accordion />
+
+      <Tabs setActiveTab={setActiveTab} />
+      {activeTab === 0 && <TabInfoOne />}
+      {activeTab === 1 && <TabInfoTwo />}
+      {activeTab === 2 && <TabInfoThree />}
+
     </div>
 
   )
